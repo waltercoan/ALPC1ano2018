9950bf5276745de592a9c8013a7cc72afcc821de
Walter Silvestre Coan <walter.coan@gmail.com>
2018-05-09 22:19:01 -0300

    aula 09/05/2018

---
+++ reajustepreco.py
@@ -0,0 +1,16 @@
+print("Digite o preço atual")
+precoatual = float(input())
+print("Digite o valor de venda medio")
+valvendmedio = float(input())
+
+if valvendmedio < 500 or precoatual < 30:
+    reajuste = precoatual + ((precoatual * 12)/100)
+else:
+    if (valvendmedio >= 500 and valvendmedio < 1600) or \
+            (precoatual >= 30 and precoatual < 80):
+        reajuste = precoatual + ((precoatual * 15)/100)
+    else:
+        if valvendmedio >= 1600 or precoatual >= 80:
+            reajuste = precoatual - ((precoatual * 25)/100)
+            
+print("O novo preco e",  reajuste)
