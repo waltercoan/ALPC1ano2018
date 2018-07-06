6921e5453e1e3fe2fde2f57ac9d498918dd14c8a
waltercoan <walter.coan@gmail.com>
2018-07-05 21:44:37 -0300

    05/07 - correção exercício blocos de repetição

--- exerprovarepeticao.py
+++ exerprovarepeticao.py
@@ -3,6 +3,9 @@ odiadamaiorqtd = 0
 
 amenorqtdpessoas = 0
 odiadamenorqtd=0
+somaeventogrande=0
+contaeventogrande=0
+contaeventopequeno = 0
 for contadia in range(8):
     print("Digite o numero do dia")
     numdia = int(input())
@@ -27,9 +30,20 @@ for contadia in range(8):
             amenorqtdpessoas = numpessoas
             odiadamenorqtd = numdia
     #aqui OHHH
-
+    if numpessoas >= 1000:
+        somaeventogrande = somaeventogrande + vallocacao
+        contaeventogrande = contaeventogrande  + 1
+    else:
+        contaeventopequeno = contaeventopequeno + 1
 #aqui nao da mais
 print("A maior qtd de pessoas e", amaiorqtdpessoas)
 print("no dia ", odiadamaiorqtd)
 print("A menor qtd de pessoas e", amenorqtdpessoas)
 print("no dia", odiadamenorqtd)
+
+print("O numero de eventos com mais de 1000 pessoas e", contaeventogrande)
+print("O numero de eventos com menos de 1000 pessoas e", contaeventopequeno)
+if contaeventogrande > 0:
+    media = somaeventogrande / contaeventogrande
+    print("A media do valor dos eventos com mais de 1000 pessoas e")
+    print(media)
