be547f68066c9b5c2abd41192d8c3ecc027afdb8
Walter Silvestre Coan <walter.coan@gmail.com>
2018-08-15 20:52:33 -0300

    09/08/2018 - lista de compras completo

--- listadecompras.py
+++ listadecompras.py
@@ -12,18 +12,44 @@ while(opcao != 9):
     print("Digite a opcao desejada")
     opcao = int(input())
     if opcao == 1:
-        print("Digite o nome do item")
-        nomeitem = input()
-        lista[proxlivre] = nomeitem
-        proxlivre = proxlivre + 1
+        if proxlivre >= 20:
+            print("Lista cheia")
+        else:
+            print("Digite o nome do item")
+            nomeitem = input()
+            achei = False
+            for i in range(20):
+                if lista[i] == nomeitem:
+                    print("Item duplicado")
+                    achei = True
+                    break
+            #if achei == False:
+            if not achei:
+                lista[proxlivre] = nomeitem
+                proxlivre = proxlivre + 1
 
     if opcao == 2:
         for i in range(20):
             print("[",i,"] - ", lista[i])
         print("Digite o numero do item para altera")
         posicao = int(input())
+        print("Digite o novo valor")
+        novonome = input()
+        lista[posicao] = novonome
+        print(lista)
+
+    if opcao == 3:
+        for i in range(20):
+            print("[",i,"] - ", lista[i])
+        print("Digite o numero do item para remover")
+        posicao = int(input())
+        lista[posicao] = ""
+        print(lista)
 
     if opcao == 4:
         for i in range(20):
             print("[", i, "] - ", lista[i], " - ", comprado[i])
+        print("Digite o numero do item para marcar comprado?")
+        posicao = int(input())
+        comprado[posicao] = "SIM"
 #aqui2
