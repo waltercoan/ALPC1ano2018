eb57bca2f47eea3ac0f6b4cc6300b93af05e5358
Walter Silvestre Coan <walter.coan@gmail.com>
2018-09-12 22:12:16 -0300

    12/09 - algoritmo do banco

---
+++ banco.py
@@ -0,0 +1,72 @@
+contas = [0] * 10
+saldos = [0] * 10
+
+for i in range(10):
+    repete = True
+    while(repete):
+        print("Digite o numero da conta")
+        novaconta = int(input())
+        achei = False
+        for j in range(10):
+            if novaconta == contas[j]:
+                print("Conta duplicada")
+                achei = True
+                repete = True
+                break
+        if not achei:
+            repete = False
+    contas[i] = novaconta
+    print("Digite o saldo da conta")
+    saldos[i] = float(input())
+
+
+
+opcao = 0
+while(opcao != 4):
+    print("Menu")
+    print("1 Efetuar deposito")
+    print("2 Efetuar saque")
+    print("3 Listar saldos ativos")
+    print("4 Sair")
+    print("Digite a opcao desejada")
+    opcao = int(input())
+
+    if opcao == 1:
+        print("DEPOSITO")
+        print("Digite o numero da conta")
+        contadep = int(input())
+        achei = False
+        for i in range(10):
+            if contadep == contas[i]:
+                print("Digite o valor do deposito")
+                valdep = float(input())
+                saldos[i] = saldos[i] + valdep
+                print("Deposito realizado com sucesso")
+                achei = True
+                break
+        if not achei:
+            print("Conta inválida...")
+
+    if opcao == 2:
+        print("SAQUE")
+        print("Digite o numero da conta")
+        contasaq = int(input())
+        achei = False
+        for i in range(10):
+            if contasaq == contas[i]:
+                achei = True
+                print("Digite o valor do saque")
+                valsaq = float(input())
+                if saldos[i] >= valsaq:
+                    saldos[i] = saldos[i] - valsaq
+                    print("Saque realizado com sucesso")
+                else:
+                    print("Saldo insuficiente")
+                break
+        if not achei:
+            print("Conta inválida...")
+
+    if opcao == 3:
+        print("Listagem de Ativos")
+        for i in range(10):
+            print(contas[i], " -> R$ ", saldos[i])
