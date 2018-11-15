b4e0d1a2d6f72ba5942eadc26b4ee57f1f317197
Walter Silvestre Coan <walter.coan@gmail.com>
2018-11-14 22:18:05 -0200

    14/11 - implementação de uma aplicação web usando flask, conceito de request - response, verbo GET e POST

--- webserver/main.py
+++ webserver/main.py
@@ -1,14 +1,25 @@
 #http://flask.pocoo.org/docs/1.0/quickstart/#a-minimal-application
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
+lista = []
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", lista=lista)
 
 @app.route("/nome/<meunome>")
 def hello(meunome):
     return "<h1> eu nao acredito</h1>" + meunome
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/formsave", methods=['POST'])
+def formsave():
+    lista.append(request.form['txtnome'])
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
